4917c751101e44167192da4d6063662b8d9980c8
LordAldi <aldianugra09@gmail.com>
2020-11-16 15:47:51 +0700

    display users from API to contact list

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,15 +1,30 @@
-import * as React from 'react';
-import {FlatList, StyleSheet} from 'react-native';
-import { View } from '../components/Themed';
-import ContactListItem from '../components/ContactListItem';
-
-import users from '../data/Users';
+import * as React from "react";
+import { FlatList, StyleSheet } from "react-native";
+import { View } from "../components/Themed";
+import ContactListItem from "../components/ContactListItem";
+import { useEffect, useState } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { listUsers } from "../graphql/queries";
 
 export default function ContactsScreen() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await API.graphql(graphqlOperation(listUsers));
+        setUsers(usersData.data.listUsers.items);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
-        style={{width: '100%'}}
+        style={{ width: "100%" }}
         data={users}
         renderItem={({ item }) => <ContactListItem user={item} />}
         keyExtractor={(item) => item.id}
@@ -21,7 +36,7 @@ export default function ContactsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
